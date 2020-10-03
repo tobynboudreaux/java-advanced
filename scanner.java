72bf8e69df1d21056f89ff64df9f6a2c5c86dfcc
tobynboudreaux <tobynboudreaux@gmail.com>
2020-10-03 16:15:48 -0500

    fixed for clearer input methods

--- scanner.java
+++ scanner.java
@@ -4,13 +4,18 @@ class MyClass {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
 
-    System.out.println("Enter name, age and salary:");
+    System.out.println("Enter name: ");
 
     // String input
     String name = myObj.nextLine();
 
+    System.out.println("Enter age: ");
+
     // Numerical input
     int age = myObj.nextInt();
+
+    System.out.println("Enter salary: ");
+
     double salary = myObj.nextDouble();
 
     // Output input by user
